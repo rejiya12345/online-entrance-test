3a00f7eeb4953d1838f26a36497fcdf90186da4c
rejiya <rejiyafersana@gmail.com>
2017-07-04 08:22:47 +0530

    file changed

---
+++ views/assets/javascripts/data.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function () {
+	console.log("data");
+
+	var url = '/user/profiledata';
+	 $.ajax(url, {
+      success: function(data) {
+      	console.log("the real data is below");
+      	console.log(data);
+      	alert("I won");
+      	$('#id_name').val(data.name);
+      	$('#id_age').val(data.age);
+      	$('#id_gender').val(data.gender);
+      	$('#id_city').val(data.city);
+      	$('#id_email').val(data.email);
+      },
+      error: function(error) {
+      	console.log("some error happened");
+      	console.log(error);
+      	alert("I failed");
+      }
+   });
+
+
+});
+
+       
+
+function doDataUpdate(){
+	var dataposturl = '/user/profileupdate';
+	 $.ajax(url, {
+      success: function(data) {
+      	console.log("the real data is below");
+      	console.log(data);
+      	alert("I won");
+      	$('#id_name').val(data.name);
+      	$('#id_age').val(data.age);
+      	$('#id_gender').val(data.gender);
+      	$('#id_city').val(data.city);
+      	$('#id_email').val(data.email);
+      },
+      error: function(error) {
+      	console.log("some error happened");
+      	console.log(error);
+      	alert("I failed");
+      }
+   });
+}
+
+$('#id_success').click(function(){
+	doDataUpdate();
+});
