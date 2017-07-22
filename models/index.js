448f47f8e36540f13aec1a597f85ea6314037e40
rejiya <rejiyafersana@gmail.com>
2017-07-22 07:25:58 +0530

    sequelize connection added

--- models/index.js
+++ models/index.js
@@ -1,34 +1,33 @@
-var postgresAdapter = require('sails-postgresql');
-var Waterline = require('waterline');
-
-var orm = new Waterline();
-
-var config = {
-  adapters: {
-    postgresql: postgresAdapter
-  },
-
-  connections: {
-    myPostgres: {
-      adapter: 'postgresql',
-      host: 'localhost',
-      user: 'ganeshdatta',
-      database: 'studygroup-express'
-    }
-  }
-};
+'use strict';
 
 var fs = require('fs');
-var path      = require("path");
+var path = require('path');
+var Sequelize = require('sequelize');
+var basename = path.basename(module.filename);
+var configFile = require(__dirname + '/../config/config');
+var env = configFile.env || process.env.NODE_ENV || 'development';
+var config = configFile[env];
+var db = {};
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
-  })
-  .forEach(function(file) {
-    var model = require(path.join(__dirname, file));
-    orm.loadCollection(model);
-  });
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-module.exports = {waterline: orm, config: config};
+fs.readdirSync(__dirname).filter(
+    function(file) {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    }).forEach(function(file) {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+});
+
+Object.keys(db).forEach(function(modelName) {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+exports.sequelize = sequelize;
